a24ceadf42dba692575ae31881be382dd13eb083
Donghee Lee <ehdgml9419@gmail.com>
2022-04-30 23:37:18 +0900

    Top Paging View

---
+++ GANMA/HomeMenuScroll/TopPagingViewController/TopPagingViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TopPagingViewController.swift
+//  GANMA
+//
+//  Created by 이동희 on 2022/04/30.
+//TODO: entity 설정 및 크기 다른 이미지 표현을 위해 collectionview 사용
+
+import Foundation
+import UIKit
+
+class TopPagingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    lazy var collectionView: UICollectionView = {
+        let collectionview = UICollectionView()
+        
+        
+        return collectionview
+    }()
+}
---
+++ GANMA/HomeMenuScroll/TopPagingViewController/TopPagingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TopPagingViewModel.swift
+//  GANMA
+//
+//  Created by 이동희 on 2022/04/30.
+//
+
+import Foundation
