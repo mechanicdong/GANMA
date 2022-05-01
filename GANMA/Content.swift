ba1dc145116db4317ef9d408097ebcd763960223
Donghee Lee <ehdgml9419@gmail.com>
2022-05-01 13:48:45 +0900

    set manga content entity

---
+++ GANMA/Content.swift
@@ -0,0 +1,41 @@
+//
+//  Content.swift
+//  GANMA
+//
+//  Created by 이동희 on 2022/05/01.
+//
+
+import UIKit
+
+struct Content: Decodable {
+    let sectionType: SectionType
+    
+    enum SectionType: String, Decodable {
+        case main //only img
+        case basic //img + description
+        case recommend // 이력 기반 추천
+        
+        var identifier: String {
+            switch self {
+            case .main:
+                return "ContentCollectionViewCell"
+            case .basic:
+                return "ContentCollectionViewBasicCell"
+            case .recommend:
+                return "ContentCollectionViewRecommendCell"
+            }
+        }
+    }
+}
+
+struct Item: Decodable {
+    let title: String
+    let description: String
+    let imageName: String
+    let summary: [String]
+    let publishedCount: [String]
+    
+    var image: UIImage {
+        return UIImage(named: imageName) ?? UIImage()
+    }
+}
